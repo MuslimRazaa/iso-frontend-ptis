49dbeb4505cbc6bf4b10a16e4ce3b605cb45cbeb
Syed Muslim Raza <syed.muslim@premier.com>
2026-06-03 14:35:51 +0500

    Frontent - with testing module2

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // API Configuration
-// export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://iso-server.ptis.co';
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://iso-server.ptis.co';
+// export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 // export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://ptis-erp-backend.onrender.com';
 
 export const API_ENDPOINTS = {
